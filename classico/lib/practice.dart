886f86e9c9edd2ae115eac1d682a52531f9ad585
Ali.Kasbati <Ali.Kasbati@gtmlho.com>
2025-08-06 17:01:07 +0500

    In progress

---
+++ classico/lib/practice.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  print("Welcome to dart!");
+  /* stdout.write("enter your name:");
+  var name = stdin.readLineSync();
+  print("Welcome, $name");*/
+
+  Human();
+
+  int a;
+  a = 5;
+  print(a);
+
+  BigInt longvalue;
+  longvalue = BigInt.parse('3123122332132132132');
+  print(longvalue);
+
+  num percentage = 99.46;
+  bool islogin = false;
+  islogin = true;
+  
+
+  a = 7;
+  print(a);
+}
+
+class Human {
+  Human();
+}
